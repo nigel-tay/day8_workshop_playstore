cc4e9af682d92698b57a49d67a081a4837d203a9
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2023-05-04 13:57:52 +0800

    first commit

---
+++ src/main/java/day8_workshop_playstore/App.java
@@ -0,0 +1,9 @@
+package day8_workshop_playstore;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/day8_workshop_playstore/AppTest.java
@@ -0,0 +1,20 @@
+package day8_workshop_playstore;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
